7cf4f24fa70d3aab9e01d54b3ef16c4919083e08
Haile-08 <haile4cmd@gmail.com>
2022-06-24 20:46:24 +0300

    if statemnt

---
+++ JS_0x1_Fundamental_1/F-1.6_if_else_statement.js
@@ -0,0 +1,17 @@
+//Assigmnet
+console.log("Fundamental part-1 if else statment");
+console.log("q.6");
+
+let langauge = "Amharic";
+const isIsland = false;
+const country = "Ethiopia";
+const continent = "africa";
+let population = 120000000;
+let finland = 6000000;
+let averagePopulation = 33000000;
+
+if(population>averagePopulation){
+    console.log(`${country} is ${population - averagePopulation} above average`)
+}else{
+    console.log(`${country} is ${population - averagePopulation} below average`)
+}
