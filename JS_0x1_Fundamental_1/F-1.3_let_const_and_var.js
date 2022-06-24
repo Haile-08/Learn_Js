62130cfd30c3f70bfcd2616e03895457dfa521a6
Haile-08 <haile4cmd@gmail.com>
2022-06-24 14:06:34 +0300

    let , const and var

---
+++ JS_0x1_Fundamental_1/F-1.3_let_const_and_var.js
@@ -0,0 +1,21 @@
+//Assigmnet
+console.log("Fundamental part-1 let,const and var");
+console.log("q.3");
+/* 
+LET
+1. let value can be changed. e.g let a = 30; then a = 31; it can be mutated
+2. let can be undifined and reassigned latter. e.g  let a;
+
+CONST
+1. const value can not be changed. is called an imutable variabe 
+2.
+
+VAR 
+basically works same as let 
+tip:-  using a changing variable can cause bugs in the futur, so always use const */
+
+let langauge = "Amharic";
+const isIsland = false;
+const country = "Ethiopia";
+const continent = "africa";
+let population = 120000000;
