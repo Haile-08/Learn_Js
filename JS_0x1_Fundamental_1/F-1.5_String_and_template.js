0f1fbd368ec13de1dd7f75e31cb9cc7bcabb9a3d
Haile-08 <haile4cmd@gmail.com>
2022-06-24 20:44:21 +0300

    string litrals

---
+++ JS_0x1_Fundamental_1/F-1.5_String_and_template.js
@@ -0,0 +1,19 @@
+//Assigmnet
+console.log("Fundamental part-1 String and template");
+console.log("q.5");
+
+/* 
+Template Literals
+1. e.g: let l = `i am ${name}`
+*/
+
+let langauge = "Amharic";
+const isIsland = false;
+const country = "Ethiopia";
+const continent = "africa";
+let population = 120000000;
+let finland = 6000000;
+let averagePopulation = 33000000;
+
+let description = `${country} is in ${continent}, and it has ${population} people who speek ${langauge} `;
+console.log(description);
