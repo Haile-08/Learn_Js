322ef0e31d7ac7051286724879029f2e3b40a713
Haile-08 <haile4cmd@gmail.com>
2022-06-24 21:02:40 +0300

    switch statemnt

---
+++ JS_0x1_Fundamental_1/F-1.10_switch_statement.js
@@ -0,0 +1,25 @@
+//Assigmnet
+console.log("Fundamental part-1 switch statement");
+console.log("q.10");
+
+let langauge = "amharic";
+
+switch(langauge){
+    case "chinese" || "mandarin":
+        console.log("MOST number of native speakers!");
+        break;
+    case  "spanish":
+        console.log("2nd place in number of native speakers");
+        break;
+    case  "english":
+        console.log("3rd place");
+        break;
+    case  "hindi":
+        console.log("Number 4");
+        break;
+    case  "arabic":
+        console.log("5th most spoken language");
+        break;
+    default:
+        console.log("Great language too :D");
+}
