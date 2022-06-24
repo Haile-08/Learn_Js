8dadf7ac0f805c4a265f113e0992433c66371faf
Haile-08 <haile4cmd@gmail.com>
2022-06-24 22:57:20 +0300

    strict mode

---
+++ JS_0x2_Fundamental_2/F-2.1_strict_mode.js
@@ -0,0 +1,10 @@
+//Assigmnet
+'use strick';
+console.log("Fundamental part-2 Data Types");
+
+/* 
+      STRICT MODE
+is used to create erro message e.g 'use strict'; on top of 
+the javascript file
+tip: always use strict mode to uncover errors
+*/
