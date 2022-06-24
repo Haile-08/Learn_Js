f3d3782dac70b849a27669dae6243af74cce6484
Haile-08 <haile4cmd@gmail.com>
2022-06-24 23:14:07 +0300

    arrow function

---
+++ JS_0x2_Fundamental_2/F-2.4_Arrow_function.js
@@ -0,0 +1,29 @@
+'use strict';
+//Assigmnet
+console.log("Fundamental part-2 Arrow Function");
+console.log("q.4");
+
+/* arrow Function 
+
+function cal(birth){
+    return 2025 - birth;
+}
+
+to 
+ 
+cal(birth) => 2025 - birth;
+
+
+
+       Function Expression 
+
+const cal = function(birth,date){
+    return 2025 - birth;
+}
+
+const cal = (birth,date) => 2025 -birth;
+*/
+
+const percentageOfWorld3 = (population)=> population*100/7900000000;
+
+console.log(percentageOfWorld3(1999));
