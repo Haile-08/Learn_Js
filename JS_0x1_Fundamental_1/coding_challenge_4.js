df264f5786764859b01fb56c07c342497923fb93
Haile-08 <haile4cmd@gmail.com>
2022-06-24 22:19:41 +0300

    coding challenge 4

---
+++ JS_0x1_Fundamental_1/coding_challenge_4.js
@@ -0,0 +1,7 @@
+/* Tip claclulor*/
+
+let bill  = prompt("Bill amount");
+let tip   = bill * 15/100;
+let total = tip + bill;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${total}`);
