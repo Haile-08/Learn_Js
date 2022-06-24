a214d7019f6d95de4729dc9daaf56a58412c4cc4
Haile-08 <haile4cmd@gmail.com>
2022-06-24 21:09:18 +0300

    Ternary operator

---
+++ JS_0x1_Fundamental_1/F-1.11_Ternary_operator.js
@@ -0,0 +1,22 @@
+//Assigmnet
+console.log("Fundamental part-1 Ternary_operator");
+console.log("q.11");
+
+/*
+
+ statement ? true : flase ;
+
+tip: we can not have if statement in a template literals but we can
+have ternary operators in a template literals
+e.g:- let n = `name is ${age>18 ? "adult":"teen"};
+*/
+let langauge = "Amharic";
+const isIsland = false;
+const country = "Ethiopia";
+const continent = "africa";
+let population = 120000000;
+let finland = 6000000;
+let averagePopulation = 33000000;
+
+console.log(`${country}'s population is ${population > 33 ? 'above' : 'below'} average`,);
+   
