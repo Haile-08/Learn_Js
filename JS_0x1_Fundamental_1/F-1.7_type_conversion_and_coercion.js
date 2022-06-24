c3c3d90af2142fcdc9f207baeb2c0042b22cce89
Haile-08 <haile4cmd@gmail.com>
2022-06-24 20:47:47 +0300

    type convertion

---
+++ JS_0x1_Fundamental_1/F-1.7_type_conversion_and_coercion.js
@@ -0,0 +1,29 @@
+//Assigmnet
+console.log("Fundamental part-1 Type conversion and coersion");
+console.log("q.7");
+
+/*    
+      type conversion
+
+Number() to convert a string to a number
+e.g: let i = "12"
+let i1 = Number(i);
+String() to convert a nummber to a string
+e.g: let i = 12;
+let l = String(i);
+
+     type coersion
+
+e.g console.log("age is "+22) so the number 22 is turned int a string automatically
+e.g console.log("22"-20)  so the string "22" is converted to numbers automatically gives 2
+e.g console.log("22"*2)   so the string "22" is converted to numbers automatically gives 44
+e.g console.log("22"/2)   so the string "22" is converted to numbers automatically gives 11
+and same goes to > or <
+
+*/
+
+console.log('9' - '5');//4
+console.log('19' - '13' + '17');//617
+console.log('19' - '13' + 17);//23
+console.log('123' < 57);//true
+console.log(5 + 6 + '4' + 9 - 4 - 2);//1143
