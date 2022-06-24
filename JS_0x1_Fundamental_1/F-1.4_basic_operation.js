c8479f55df10c33c6a4d2e53e615729be9b1c681
Haile-08 <haile4cmd@gmail.com>
2022-06-24 15:47:55 +0300

    basic operator

---
+++ JS_0x1_Fundamental_1/F-1.4_basic_operation.js
@@ -0,0 +1,18 @@
+//Assigmnet
+console.log("Fundamental part-1 Basic Operation");
+console.log("q.4");
+
+let langauge = "Amharic";
+const isIsland = false;
+const country = "Ethiopia";
+const continent = "africa";
+let population = 120000000;
+let finland = 6000000;
+let averagePopulation = 33000000;
+let description = country + " is in "+continent+", and it has "+population+" people who speek "+langauge;
+
+console.log("Population split in to 2 :-"+population/2);
+console.log("populatoin increased by 1 :-"+population+1);
+console.log(population>finland);
+console.log(population<averagePopulation);
+console.log(description);
