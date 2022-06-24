fb71fdaf22a561bafa46151ad2a2ec7b76f6f5f6
Haile-08 <haile4cmd@gmail.com>
2022-06-24 23:04:01 +0300

    functions

---
+++ JS_0x2_Fundamental_2/F-2.2_Functions.js
@@ -0,0 +1,9 @@
+//Assigmnet
+console.log("Fundamental part-2 Data Types");
+console.log("q.1");
+
+function describeCountry(country,population,capitalCity){
+    console.log(`${country} has ${population} people and its capital city is ${capitalCity}`);
+
+}
+describeCountry("ethiopia",12000000,"addis abeba")
