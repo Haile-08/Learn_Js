6265f261393f762f31e2cc5d7a1c92d2db04b7fa
Haile-08 <haile4cmd@gmail.com>
2022-06-24 13:36:54 +0300

    fundamental

---
+++ JS_0x1_Fundamental_1/F-1.1_Values_and_Variables_v-10.js
@@ -0,0 +1,15 @@
+//Assigmnet
+console.log("Fundamental part-1 values and variables");
+console.log("q.1");
+let country = "Ethiopia";
+let continent = "africa";
+let population = "120000000";
+console.log("country "+country);
+console.log("continent "+continent);
+console.log("population "+population);
+/*
+Varibale naming convention
+
+1. camel case : e.g let firstNamePerson the first letter is samll then the other ara capital
+
+2. name constants in all upper case :  e.g const PI = 3.1451*/
