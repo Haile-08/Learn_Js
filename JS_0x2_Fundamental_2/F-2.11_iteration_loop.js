83cbd109369454af5031d3be9c8a2373f1381b69
Haile-08 <haile4cmd@gmail.com>
2022-06-25 09:33:34 +0300

    for loop

---
+++ JS_0x2_Fundamental_2/F-2.11_iteration_loop.js
@@ -0,0 +1,8 @@
+'use strict';
+//Assigmnet
+console.log("Fundamental part-2 iteration for loop");
+console.log("q.10");
+
+for(let i=1;i<=50;i++){
+    console.log(`'Voter number ${i} is currently voting`);
+}
