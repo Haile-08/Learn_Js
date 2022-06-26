e91736138b2c4d9db19becc1dc42a83e75a74886
Haile-08 <haile4cmd@gmail.com>
2022-06-26 10:36:54 +0300

    short circuting

---
+++ JS_0X04_Modern_js/MJ_0x4_short_circuting.js
@@ -0,0 +1,9 @@
+//short curicuting
+// || returns the first true value
+
+console.log(3 || "haile");
+console.log("" || "haile");
+console.log(true || 0);
+console.log(undefined || 0);
+// && returns if the first and secode values are true
+console.log(0 && "haile");
