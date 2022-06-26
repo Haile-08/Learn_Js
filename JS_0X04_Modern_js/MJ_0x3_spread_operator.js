65c266fd710b125f98e0a072621626d990846494
Haile-08 <haile4cmd@gmail.com>
2022-06-26 10:02:55 +0300

    spread operator

--- JS_0X04_Modern_js/MJ_0x3_spread_operator.js
+++ JS_0X04_Modern_js/MJ_0x3_spread_operator.js
@@ -7,3 +7,13 @@ let newarray = [4, 5, ...arry];
 //output  4,5,0,1,2
 let newar = [4, 5, arry];
 //output 4,5,[0,1,2]
+console.log(...newar);
+
+const str = "haile";
+console.log(...str);
+console.log(`  ${...str} `); //doesnot work in string litral
+
+//Rest 
+const [a,b,...others] = [1,2,3,4,5,6];
+console.log(a,b,others);
+//output 1 2 [3 4 5 6]
