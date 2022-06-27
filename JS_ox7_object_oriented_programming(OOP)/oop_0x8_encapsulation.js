1c215a52a15d620be259313890e0c156d15209f5
Haile-08 <haile4cmd@gmail.com>
2022-06-27 16:11:34 +0300

    encapsulation

---
+++ JS_ox7_object_oriented_programming(OOP)/oop_0x8_encapsulation.js
@@ -0,0 +1,36 @@
+"use strict";
+
+class Person {
+  //constractor method
+  constructor(fullname, birthyear) {
+    // lets protect fullname call it protected
+    this.fullname = fullname;
+    this.birthyear = birthyear;
+  }
+  clacage() {
+    console.log(2014 - this.birthyear);
+  }
+  get age() {
+    return 2014 - this.birthyear;
+  }
+  set fullNAME(name) {
+    if (name.includes(" ")) {
+      this._fullNAME = name;
+    } else {
+      alert(`${name} this is not a full name`);
+    }
+  }
+
+  // we use the keyword static to create a static method
+
+  static hey() {
+    console.log("hey there");
+  }
+
+  //ecapsulation
+  //**************************** */
+  getfullname() {
+    return this.fullname;
+  }
+  //they can get the fullname but not edit it
+}
