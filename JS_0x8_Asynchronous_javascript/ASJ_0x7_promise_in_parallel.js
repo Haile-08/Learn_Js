3ec3bd2bcf0694be3f9e996bdb0058d831cfcdc8
Haile-08 <haile4cmd@gmail.com>
2022-07-05 20:43:25 +0300

    Runnning Promises in parallel

---
+++ JS_0x8_Asynchronous_javascript/ASJ_0x7_promise_in_parallel.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//  Running Promises in parallel
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+    // to run in parallel
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+    console.log(data);
+    console.log(data.map((d) => d[0].capital));
+  } catch (error) {
+    console.error(error);
+  }
+};
