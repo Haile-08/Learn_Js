3fbdd75a3546835b11309d04271bded833e61489
Haile-08 <haile4cmd@gmail.com>
2022-07-05 19:51:16 +0300

    error handling with async await

--- JS_0x8_Asynchronous_javascript/ASJ_0x6_consuming_promis_with_async_await.js
+++ JS_0x8_Asynchronous_javascript/ASJ_0x6_consuming_promis_with_async_await.js
@@ -16,3 +16,25 @@ const whereami = async function (country) {
 };
 
 whereami("ethiopia");
+
+
+// error handling with async await catch
+
+const whereamib = async function (country) {
+    try{
+    const rest = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+    const data = await rest.json();
+  
+    console.log(data);
+  
+    //This is same as
+  
+    //   fetch(`https://restcountries.com/v3.1/name/${country}`)
+    //     .then(function (response) {
+    //       console.log(response);
+    //       return response.json();
+    //     })
+    }catch(err){
+        console.log(err);
+    }
+  };
