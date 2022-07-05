f749a6a65d99437db54a30dedc8ac3d9d118e8d7
Haile-08 <haile4cmd@gmail.com>
2022-07-05 13:55:55 +0300

    promises

---
+++ JS_0x8_Asynchronous_javascript/ASJ_0x5_creating_a_promise.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const lotto = new Promise(function (resolve, reject) {
+  console.log("loto is beaing drawen");
+  setTimeout(() => {
+    if (Math.random() >= 0.5) {
+      resolve("u win");
+    } else {
+      reject(new Error("u loss"));
+    }
+  }, 2000);
+});
+// consuming promise
+lotto.then(res => console.log(rse)).catch(err => console.log(err);)
+
+
+//promisifying setTimeout
+
+const wait = function(sec){
+    return new Promise(function(resolve){
+        setTimeout(resolve, seconds * 1000)
+    })
+}
+
+
+//consuming promise
+wait(2).then(()=>{
+    console.log(' i waited for 2 sec')
+    return wait(1);
+}).then(()=> console.log('i waited for 1 sec'))
