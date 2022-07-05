c3dda90a1247e69059e6ddb03ed7a2374d026991
Haile-08 <haile4cmd@gmail.com>
2022-07-05 20:27:21 +0300

    returning value from async await

--- JS_0x8_Asynchronous_javascript/ASJ_0x6_consuming_promis_with_async_await.js
+++ JS_0x8_Asynchronous_javascript/ASJ_0x6_consuming_promis_with_async_await.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// Async  await
+
+
 const whereami = async function (country) {
   const rest = await fetch(`https://restcountries.com/v3.1/name/${country}`);
   const data = await rest.json();
@@ -38,3 +41,44 @@ const whereamib = async function (country) {
         console.log(err);
     }
   };
+
+  //returning values from async awit function
+
+
+const whereamibi = async function (country) {
+    try{
+    const rest = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+    const data = await rest.json();
+  
+    console.log(data);
+    
+     return `you are in ${data.city}`
+    //This is same as
+  
+    //   fetch(`https://restcountries.com/v3.1/name/${country}`)
+    //     .then(function (response) {
+    //       console.log(response);
+    //       return response.json();
+    //     })
+    }catch(err){
+        console.log(err);
+    }
+  };
+
+
+  // returning the function
+
+  whereamibi()
+    .then((city) => console.log(city))
+    .catch((err) => console.log(err))(
+    // easier method
+    async function () {
+      try {
+        const city = await whereamibi();
+        console.log(`2: ${city}`);
+      } catch (error) {
+        console.error(`2 : ${error.message}`);
+      }
+      console.log(`3: Finished getting loaction`);
+    }
+  );
